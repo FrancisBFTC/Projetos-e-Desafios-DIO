cdbc64d4f7b0cade4e06541423e1a9c17059b98a
FrancisBFTC <bftcorporations@gmail.com>
2022-02-27 16:39:31 -0300

    Novos arquivos para estudo do BootCamp

---
+++ Curso_Programacao_Para_Internet/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Meu primeiro site em PHP! Yes!</title>
+	</head>
+	<body>
+		<?php
+			for($i = 0; $i < 10; $i++){
+				print("Número :" . $i . "<br />");
+			}
+		?>
+
+	</body>
+</html>
