83fecc39f392a29288dde02aefa11e40ab39b013
FrancisBFTC <bftcorporations@gmail.com>
2022-03-08 22:09:08 -0300

    Fazendo novos testes JS

---
+++ Curso - Cursos de JavaScript/Sintaxe Básica em JavaScript/Aula 5 - Funcoes/script.js
@@ -0,0 +1,12 @@
+function funcao(){
+    console.log("This message comes from function!");
+}
+
+funcao();
+
+
+function message(first, second){
+    console.log(first, second);
+}
+
+message('Hello World!', 'Hands-on, Friends!');
