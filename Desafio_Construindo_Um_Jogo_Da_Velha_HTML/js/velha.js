54247d400b97d24eae8fd594cec16aec09b909dc
FrancisBFTC <bftcorporations@gmail.com>
2022-03-27 20:07:49 -0300

    Verifica se quadrado já foi pressionado e troca jogador

--- Desafio_Construindo_Um_Jogo_Da_Velha_HTML/js/velha.js
+++ Desafio_Construindo_Um_Jogo_Da_Velha_HTML/js/velha.js
@@ -4,10 +4,15 @@ var selectedPlayer = document.getElementById("jogador-selecionado");
 exchangePlayer("X");
 
 function chooseSquare(id){
+    
     let square = document.getElementById(id);
+    if(square.innerHTML !== '-') return;
 
     square.innerHTML = player;
     square.style.color = "#000";
+
+    player = (player === "X") ? "O" : "X";
+    exchangePlayer(player);
 }
 
 function exchangePlayer(value){
