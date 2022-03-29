4b4e27b60a8a089cff4b8a1ef6af582f1dc07048
FrancisBFTC <bftcorporations@gmail.com>
2022-03-29 20:29:44 -0300

    Funções para clique das cores e checagem da ordem correta

--- Desafio_Criando_Seu_Jogo_de_Memória_Estilo_Genius/js/main.js
+++ Desafio_Criando_Seu_Jogo_de_Memória_Estilo_Genius/js/main.js
@@ -12,6 +12,7 @@ const RED_DIV    = document.querySelector('.red');
 const GREEN_DIV  = document.querySelector('.green');
 const YELLOW_DIV = document.querySelector('.yellow');
 
+// Esta função cria ordem aleatória de cores
 let shuffleOrder = () => {
 	let colorOrder = Math.floor(Math.ramdom() * 4);
 	order[order.length] = colorOrder;
@@ -23,6 +24,7 @@ let shuffleOrder = () => {
 	}
 }
 
+// Torna mais clara a próxima cor
 let lightColor = (element, number) => {
 	number = number * 500;
 	setTimeOut(() => {
@@ -32,4 +34,31 @@ let lightColor = (element, number) => {
 	setTimeOut(() => {
 		element.classList.remove('selected');
 	});
+}
+
+// Identifica se os botões clicados é a mesma ordem do jogo
+let checkOrder = () => {
+	for(let i in clickedOrder){
+		if(clickedOrder[i] != order[i]){
+			gameover();
+			break;
+		}
+	}
+
+	if(clickedOrder.length == order.length){
+		alert(`Seus pontos são ${score}\n Você acertou! Iniciando próximo nível`);
+		nextLevel();
+	}
+}
+
+// Função para cliques dos usuários
+let click = (color) => {
+	clickedOrder[clickedOrder.length] = color;
+	createColorElement(color).classList.add('selected');
+
+	setTimeOut(() => {
+		createColorElement(color).classList.remove('selected');
+	});
+
+	checkOrder();
 }
