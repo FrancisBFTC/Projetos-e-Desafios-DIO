c4d471d6ac8b55725d2bd3256005ead116fedc8f
FrancisBFTC <bftcorporations@gmail.com>
2022-05-09 22:04:01 -0300

    Configuração Webpack + babel + devserver + html plugins

---
+++ Introducao-ao-ReactJS/_webpack/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Bem vinda a nossa aula :D</p>
+            <p> Webpack 4</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ Introducao-ao-ReactJS/_webpack/teste.js
@@ -0,0 +1,7 @@
+console.log('Hello World')
+let value = 'Hello World'
+
+if(value === 'Hello World')
+	console.log('String correta')
+else
+	console.log('String incorreta')
